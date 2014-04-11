0bd973a9c0d8ffed386e8688ac10c4ab586f1be8
rmarzochi <rmarzochi@localhost.localdomain>
2014-04-11 00:07:19 -0300

    start project

---
+++ LocalizacaoGeo-WS/src/main/java/com/usp/LocalizacaoGeo_WS/App.java
@@ -0,0 +1,13 @@
+package com.usp.LocalizacaoGeo_WS;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World git!" );
+    }
+}
